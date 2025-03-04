123a7eb7b00b21ef150eb4568646e87a951fbc28
Shashwat <shashwatk2006@gmail.com>
2025-03-04 14:07:33 +0530

    querySelectorAll

--- dom/adding-changing/add.js
+++ dom/adding-changing/add.js
@@ -7,7 +7,10 @@ newh1.style.color = "red"
 newh1.style.fontFamily = "helvetica"
 newh1.style.textAlign = "center"
 // STEP 3 APPEND ELEMENT TO DOM
-document.body.append(newh1)
+// document.body.append(newh1)
 // using prepend instead of append adds the element to the starting of the page (being the first child)
 // document.getElementById("box1").append(newh1) 
 // this line will add newh1 to box1 in the end text
+
+const boxes = document.querySelectorAll(".box")
+document.body.insertBefore(newh1, boxes)
